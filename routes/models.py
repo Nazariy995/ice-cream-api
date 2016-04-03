7c029ebf6272e46ba4df518e2b573cd47cd1c939
nibrahim22 <nibrahim22@gmail.com>
2016-04-03 18:04:53 +0000

    corrected the models

--- routes/models.py
+++ routes/models.py
@@ -1,3 +1,12 @@
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Route(models.Model):
+    route_number = models.IntegerField(max_length = 4)
+    city_list = models.CharField(max_length = 30)
+    trucks = models.IntegerField(max_length = 4)
+    date_added = models.DateTimeField(auto_now_add=True, editable=False)
+    date_updated = models.DateTimeField(auto_now=True)
--- trucks/models.py
+++ trucks/models.py
@@ -1,3 +1,10 @@
+
 from django.db import models
 
 # Create your models here.
+
+class Truck(models.Model):
+    truck_number = models.IntegerField(max_length = 4)
+    available = models.NullBooleanField
+    model = models.CharField(max_length = 50)
+    year = models.IntegerField(max_length = 4)
