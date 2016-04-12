1b01a4a1600ab1d1f24df9d8a9419629fe0c11dd
nibrahim22 <nibrahim22@gmail.com>
2016-04-12 01:18:16 +0000

    Added user

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 from api.routes import views as routesViews
+from rest_framework.authtoken import views
 
 urlpatterns = patterns('',
 
-                       url(r'^routes/$', routesViews.Routes.as_view())
-
-
+                       url(r'^routes/$', routesViews.Routes.as_view()),
+                       url(r'^login/$', views.obtain_auth_token),
 )
