975f73bd9c2b20514cc106144cec23da58dbef18
nibrahim22 <nibrahim22@gmail.com>
2016-04-16 17:23:22 +0000

    Added Upload views for uploading

---
+++ api/upload/views.py
@@ -0,0 +1,7 @@
+from rest_framework import status
+from rest_framework.decorators import permission_classes
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+
