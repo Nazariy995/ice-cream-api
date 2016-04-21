fc38e4144ad36b4cb378311579e8631bae1026b1
Nazariy995 <nazariy1995@gmail.com>
2016-04-21 16:07:06 -0400

    Added file upload

--- api/upload/views.py
+++ api/upload/views.py
@@ -1,7 +1,23 @@
 from rest_framework import status
-from rest_framework.decorators import permission_classes
+from rest_framework.decorators import permission_classes, parser_classes
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 
 
+class Upload(APIView):
+    permission_classes=(IsAuthenticated,)
+    parser_classes = (MultiPartParser, FormParser,)
+
+    def put(self, request, format="txt"):
+
+        file_obj = request.FILES["file"]
+        lines = file_obj.__iter__()
+        for line in lines:
+            print line
+        msg = {
+            "result":"Technical dificulties!"
+        }
+
+        return Response(msg, status=status.HTTP_200_OK)
--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from api.routes import views as routesViews
+from api.upload import views as uploadViews
 from rest_framework.authtoken import views
 
 urlpatterns = patterns('',
 
                        url(r'^routes/$', routesViews.Routes.as_view()),
+                       url(r'^upload/$', uploadViews.Upload.as_view()),
                        url(r'^login/$', views.obtain_auth_token),
 )
