8ee6e1e56a9d9748c63313bb830d11fdf5b70f88
Nazariy995 <nazariy1995@gmail.com>
2016-04-26 04:38:32 -0400

    Added logging truck route upload

--- api/upload/load_truck_route.py
+++ api/upload/load_truck_route.py
@@ -6,6 +6,8 @@ from truck_route.models import TruckRoute
 from routes.models import Route
 from trucks.models import Truck
 from datetime import datetime
+import logging
+log = logging.getLogger('ice_cream_api')
 
 class LoadTruckRoute:
 
@@ -17,29 +19,36 @@ class LoadTruckRoute:
         #Traverse the trucks line by line
         date = self.load_date(truck_route_file[0])
         count = 0
-        for data in truck_route_file[1:-1]:
-            count += 1
-            try:
-                truck_route = {}
-                truck_route["truck_number"] = int(data[TRUCK_NUM_S:TRUCK_NUM_E])
-                truck_route["route_number"] = int(data[ROUTE_NUM_S:ROUTE_NUM_E])
-                truck_route["date_added"] = date
-                #Check if the truck number exists
-                if not Truck.objects.filter(truck_number=truck_route["truck_number"]):
-                    raise ValueError("Truck number {} does not exist")
-                #Check if the route number exists
-                if not Route.objects.filter(route_number=truck_route["route_number"]):
-                    raise ValueError("Route number {} does not exist")
+        try:
+            for data in truck_route_file[1:-1]:
+                count += 1
                 try:
-                    #Save the truck route to the database
-                    db_truck_route = TruckRoute.objects.create(**truck_route)
-                except IntegrityError as err:
-                    error = "Rout {} is already assigned to truck {}".format(truck_route["route_number"], truck_route["truck_number"])
+                    truck_route = {}
+                    truck_route["truck_number"] = int(data[TRUCK_NUM_S:TRUCK_NUM_E])
+                    truck_route["route_number"] = int(data[ROUTE_NUM_S:ROUTE_NUM_E])
+                    truck_route["date_added"] = date
+                    #Check if the truck number exists
+                    if not Truck.objects.filter(truck_number=truck_route["truck_number"]):
+                        raise ValueError("Truck number {} does not exist".format(truck_route["truck_number"]))
+                    #Check if the route number exists
+                    if not Route.objects.filter(route_number=truck_route["route_number"]):
+                        raise ValueError("Route number {} does not exist".format(truck_route["route_number"]))
+                    try:
+                        #Save the truck route to the database
+                        db_truck_route = TruckRoute.objects.create(**truck_route)
+                    except IntegrityError as err:
+                        error = "Rout {} is already assigned to truck {}".format(truck_route["route_number"], truck_route["truck_number"])
+                        log.error(error)
+                        errors["data"].append(error)
+                except ValueError as err:
+                    error = str(err)
+                    log.error(error)
                     errors["data"].append(error)
-            except ValueError as err:
-                error = str(err)
-                errors["data"].append(error)
+        except Exception as e:
+            log.error(str(e))
+            errors["data"].append("Please check the formatting of the file")
 
+        log.info("Updated Truck Routes")
         #Check if the trailer count matches
         errors["trailer"] += self.load_trailer(truck_route_file[-1], count)
         return errors
@@ -60,6 +69,7 @@ class LoadTruckRoute:
 
         except ValueError as err:
             errors.append(str(err))
+            log.error(str(err))
 
         return errors
 
