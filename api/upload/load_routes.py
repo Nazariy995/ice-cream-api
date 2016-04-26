260e5ba114c4b1e06e6b8cff3547026a5bf3e073
Nazariy995 <nazariy1995@gmail.com>
2016-04-26 03:26:30 -0400

    Added error checking and logging to routes

--- api/upload/load_routes.py
+++ api/upload/load_routes.py
@@ -3,6 +3,8 @@ from constants.trailer import *
 from cities.models import City
 from routes.models import Route
 from django.db.models import Q
+import logging
+log = logging.getLogger('ice_cream_api')
 
 class LoadRoutes:
 
@@ -17,30 +19,37 @@ class LoadRoutes:
         errors["trailer"] = []
         count = 0
         #Traverse the routes line by line
-        for line in route_file[:-1]:
-            count += 1
-            temp_errors = []
-            action = line[ACTION_S:ACTION_E]
-            route_number = int(line[ROUTE_N_S:ROUTE_N_E])
-            cities = []
-            city_start_location = CITIES_S
-            while city_start_location < len(line):
-                city_end_location = city_start_location + CITIES_L
-                city = line[city_start_location:city_end_location].strip()
-                if city:
-                    cities.append(city)
-                city_start_location = city_end_location
+        try:
+            for line in route_file[:-1]:
+                count += 1
+                temp_errors = []
+                action = line[ACTION_S:ACTION_E]
+                route_number = int(line[ROUTE_N_S:ROUTE_N_E])
+                cities = []
+                city_start_location = CITIES_S
+                while city_start_location < len(line):
+                    city_end_location = city_start_location + CITIES_L
+                    city = line[city_start_location:city_end_location].strip()
+                    if city:
+                        cities.append(city)
+                    city_start_location = city_end_location
 
-            if action in ACTIONS:
-                func = ACTIONS.get(action, None)
-                temp_errors += func(cities, route_number)
-            else:
-                error = "Action for route number {} is not allowed"
-                temp_errors.append(error)
+                if action in ACTIONS:
+                    func = ACTIONS.get(action, None)
+                    temp_errors += func(cities, route_number)
+                else:
+                    error = "Action for route number {} is not allowed"
+                    log.error(error)
+                    temp_errors.append(error)
 
-            errors["data"] += temp_errors
+                errors["data"] += temp_errors
+        except Exception as e:
+            log.error(str(e))
+            errors["data"].append("Please check the formatting of the file")
 
         errors["trailer"] += self.load_trailer(route_file[-1], count)
+
+        log.info("Routes Updated")
 
         return errors
 
@@ -58,16 +67,20 @@ class LoadRoutes:
                     db_cities.update(route=route)
                 else:
                     error = "ADD: One of the cities in route number {} does not exist or is already assigned".format(route_number)
+                    log.error(error)
                     errors.append(error)
             else:
                 error = "ADD: Route number {} already exists in the database".format(route_number)
+                log.error(error)
                 errors.append(error)
         else:
             error = "ADD: Route number {} either contains more than {} or not enough cities".format(route_number, MAX_CITIES)
+            log.error(error)
             errors.append(error)
 
         return errors
 
+    #Change Route
     def change_route(self, cities, route_number):
         errors = []
         if len(cities) <= MAX_CITIES and len(cities) > 0:
@@ -80,12 +93,15 @@ class LoadRoutes:
                     db_cities.update(route=route)
                 else:
                     error = "CHANGE: One of the cities is not available for route number {}".format(route_number)
+                    log.error(error)
                     errors.append(error)
             else:
                 error = "CHANGE: Route number {} does not exists in the database".format(route_number)
+                log.error(error)
                 errors.append(error)
         else:
             error = "CHANGE: Route number {} either contains more than {} or not enough cities".format(route_number, MAX_CITIES)
+            log.error(error)
             errors.append(error)
 
         return errors
@@ -98,9 +114,11 @@ class LoadRoutes:
                 route.delete()
             else:
                 error = "DELETE: Route number {} does not exist".format(route_number)
+                log.error(error)
                 errors.append(error)
         else:
             error = "DELETE: Route number {} contains cities that should not be there".format(route_number)
+            log.error(error)
             errors.append(error)
 
         return errors
@@ -115,6 +133,7 @@ class LoadRoutes:
                 raise ValueError("Trailer count does not match. Please Fix it in the file.")
 
         except ValueError as err:
+            log.error(error)
             errors.append(str(err))
 
         return errors
